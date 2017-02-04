b9d4159a3c1edbe8dfc35477764d76e1ecca7ee6
Anubhav Tomar <anubhav.tomar94@gmail.com>
2017-02-04 20:55:54 +0530

    problems done (#2)

--- problems/tasks/app.js
+++ problems/tasks/app.js
@@ -14,59 +14,102 @@ process.stdin.on("end", function () {
 
 /*     
 -----------------------------------------------------------------------------
-PROBLEM 1
+PROBLEM 1 (Uncomment the code below to run the solution)
 -----------------------------------------------------------------------------
 */
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
 
 function processData () {
 	_input = _input.split('\n');
-	var len = _input[0];
-	for(var i = 0 ; i < len ; i++){
-		process.stdout.write(eval(_input[i+1]) + ' \n');
-	}
-}
-
-/*     
------------------------------------------------------------------------------
-PROBLEM 2
------------------------------------------------------------------------------
-*/
-
-function processData () {
-	_input = _input.split('\n');
-	var len = _input[0];
-	for(var i = 0 ; i < len ; i++){
-		process.stdout.write(eval(_input[i+1]) + ' \n');
-	}
-}
-
-/*     
------------------------------------------------------------------------------
-PROBLEM 3 and 4
------------------------------------------------------------------------------
-*/
-
-String.prototype.XyloHack = function (n) {
-	if(n % 2)
-		return this.toLowerCase();
-
-	return this.toUpperCase();
-};
-
-
-function add(x, y){
-	if(!y){
-		return function(y){
-			return x + y;
+	var len = _input.length , fileName = _input[0].split('.')[0] , 
+		ext = _input[0].split('.')[1] , fileContent = [] , readingFileContent = true;
+	console.log(_input);
+	for(var i = 1 ; i < len ; i++){
+		if(readingFileContent){
+			if(_input[i] == '' && _input[i+2] == ''){
+				if(i+2 == len-1)
+					break;
+				readingFileContent = false;
+			}
+			else {
+				_input[i] = _input[i].replace(/[$]/g, '');
+				fileContent.push(_input[i]);
+			}
 		}
-	}
-	return x + y;
+		else if(_input[i]){
+			fileName += capitalizeFirstLetter(_input[i].split('.')[0]);
+			readingFileContent = true;
+		}
+	}		
+	process.stdout.write(fileName + '.' + ext + ' \n');
+	len = fileContent.length
+	for(var i = 0 ; i < len ; i++)
+		process.stdout.write(fileContent[i] + ' \n');
 }
 
-function processData () {
-	_input = _input.split('\n');
-	var len = _input[0];
-	for(var i = 0 ; i < len ; i++){
-		process.stdout.write(eval(_input[i+1]) + ' \n');
-	}
-}
+/*     
+-----------------------------------------------------------------------------
+PROBLEM 2 (Uncomment the code below to run solution)
+-----------------------------------------------------------------------------
+*/
+
+// function getCardType(number) {
+// 	var visaRegex = new RegExp('^4[0-9]{0,}$');
+// 	var masterRegex = new RegExp('^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01]|2720)[0-9]{0,}$');
+// 	var discoverRegex = new RegExp('^(6011|65|64[4-9]|62212[6-9]|6221[3-9]|622[2-8]|6229[01]|62292[0-5])[0-9]{0,}$');
+// 	var amExRegex = new RegExp('^3[47][0-9]{0,}$');
+// 	number = number.replace(/ /g ,"");
+
+// 	if(amExRegex.test(number))
+// 		return 'American Express';
+// 	else if(visaRegex.test(number))
+// 		return 'Visa';
+// 	else if(masterRegex.test(number))
+// 		return 'Mastercard';
+// 	else if(discoverRegex.test(number))
+// 		return 'Discover';
+// 	else
+// 		return 'Invalid';
+// }
+
+// function processData () {
+// 	_input = _input.split('\n');
+// 	var len = _input[0];
+// 	for(var i = 0 ; i < len ; i++){
+// 		process.stdout.write(getCardType(_input[i+1]+'') + ' \n');
+// 	}
+// }
+
+/*     
+-----------------------------------------------------------------------------
+PROBLEM 3 and 4 (Uncomment the code below to run both the problems)
+-----------------------------------------------------------------------------
+*/
+
+// String.prototype.XyloHack = function (n) {
+// 	if(n % 2)
+// 		return this.toLowerCase();
+
+// 	return this.toUpperCase();
+// };
+
+
+// function add(x, y){
+// 	if(!y){
+// 		return function(y){
+// 			return x + y;
+// 		}
+// 	}
+// 	return x + y;
+// }
+
+// function processData () {
+// 	_input = _input.split('\n');
+// 	var len = _input[0];
+// 	for(var i = 0 ; i < len ; i++){
+// 		process.stdout.write(eval(_input[i+1]) + ' \n');
+// 	}
+// }
